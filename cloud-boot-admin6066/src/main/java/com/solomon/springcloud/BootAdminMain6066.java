f2f0bc8c9dce1dd8be83fff32374866293e4e809
陈健夫 <chenjianfu@yungehuo.com>
2021-02-05 16:08:05 +0800

    加入spring boot Admin

---
+++ cloud-boot-admin6066/src/main/java/com/solomon/springcloud/BootAdminMain6066.java
@@ -0,0 +1,14 @@
+package com.solomon.springcloud;
+
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class BootAdminMain6066 {
+    public static void main(String[] args) {
+        SpringApplication.run(BootAdminMain6066.class, args);
+    }
+}
