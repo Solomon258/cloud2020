116a6e3a5fa619b6d4234f2d59b3e18ecde6464b
陈健夫 <chenjianfu@yungehuo.com>
2021-02-20 18:16:05 +0800

    SpringBoot Admin

--- cloud-consumer-order80/src/main/java/com/solomon/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/solomon/springcloud/OrderMain80.java
@@ -3,11 +3,13 @@ package com.solomon.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class OrderMain80 {
-
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
     }
--- cloud-consumer-order80/src/main/java/com/solomon/springcloud/controller/OderController.java
+++ cloud-consumer-order80/src/main/java/com/solomon/springcloud/controller/OderController.java
@@ -1,17 +1,26 @@
 package com.solomon.springcloud.controller;
 
+import com.netflix.discovery.shared.Applications;
 import com.solomon.springcloud.entities.CommonResult;
 import com.solomon.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
+@Slf4j
 public class OderController {
 
     @Resource
     private RestTemplate restTemplate;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
 //    private final static String PAYMENT_URI = "http://localhost:8001";
     private final static String PAYMENT_URI = "http://cloud-payment-service";
@@ -25,4 +34,22 @@ public class OderController {
     public CommonResult<Payment> get(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PAYMENT_URI + "/paymnet/getPaymentById/"+id,  CommonResult.class);
     }
+
+    @GetMapping("/consumer/discovery/info")
+    public Object info() {
+        //获取 eureka server 端的所有服务
+        List<String> services =  discoveryClient.getServices();
+        for (String service :  services) {
+            List<ServiceInstance> instances = discoveryClient.getInstances(service);
+            log.info("service is {}", service);
+            //获取所有实例的信息
+            for (ServiceInstance serviceInstance :instances ) {
+                log.info("instanceId:{}, serviceId:{}, URI:{}, PORT:{}"
+                        , serviceInstance.getInstanceId(), serviceInstance.getServiceId(),serviceInstance.getUri(),serviceInstance.getPort());
+            }
+        }
+        return services;
+    }
+
+
 }
